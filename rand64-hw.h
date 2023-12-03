cfeca024c00644a0e5bddac555d5b865bc4b091c
DENG <marvind@lnxsrv15.seas.ucla.edu>
2023-12-02 19:33:39 -0800

    refactored hardware functions

---
+++ rand64-hw.h
@@ -0,0 +1,18 @@
+#ifndef RAND64_HW_H
+#define RAND64_HW_H
+
+#include <stdbool.h>
+
+struct cpuid;
+
+struct cpuid cpuid (unsigned int leaf, unsigned int subleaf);
+
+_Bool rdrand_supported (void);
+
+void hardware_rand64_init(void);
+
+unsigned long long hardware_rand64(void);
+
+void hardware_rand64_fini(void);
+
+#endif
