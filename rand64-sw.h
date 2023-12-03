5984f49c60e85481d8e7c7387b003c1992dcc8f3
DENG <marvind@lnxsrv15.seas.ucla.edu>
2023-12-02 19:37:51 -0800

    refactored software functions

---
+++ rand64-sw.h
@@ -0,0 +1,17 @@
+#ifndef RAND64_SW_H
+#define RAND64_SW_H
+
+#include <stdio.h>
+
+void initfile(char* file);
+
+void software_rand64_init(void);
+
+void software_rand64_init(void);
+
+unsigned long long software_rand64(void);
+
+void software_rand64_fini(void);
+
+
+#endif 
--- randall.c
+++ randall.c
@@ -32,38 +32,8 @@
 
 #include "options.h"
 #include "rand64-hw.h"
+#include "rand64-sw.h"
 
-
-/* Software implementation.  */
-
-/* Input stream containing random bytes.  */
-static FILE *urandstream;
-
-/* Initialize the software rand64 implementation.  */
-static void
-software_rand64_init (void)
-{
-  urandstream = fopen ("/dev/random", "r");
-  if (! urandstream)
-    abort ();
-}
-
-/* Return a random value, using software operations.  */
-static unsigned long long
-software_rand64 (void)
-{
-  unsigned long long int x;
-  if (fread (&x, sizeof x, 1, urandstream) != 1)
-    abort ();
-  return x;
-}
-
-/* Finalize the software rand64 implementation.  */
-static void
-software_rand64_fini (void)
-{
-  fclose (urandstream);
-}
 
 static bool
 writebytes (unsigned long long x, int nbytes)
